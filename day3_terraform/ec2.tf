ee1cd823a627a5b7f2670b7e16af37052efa74b4
veeranjanayulu <141625717+veeranjanayulu@users.noreply.github.com>
2024-09-04 11:12:06 +0530

    Update ec2.tf

--- day3_terraform/ec2.tf
+++ day3_terraform/ec2.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "name" {
     ami = "ami-07e35c3920b92d884"
     instance_type = "t2.micro"
     key_name = "react"
-    subnet_id = aws_subnet.public.id
-    vpc_security_group_ids = [aws_security_group.secure.id]
+    
   
 }
