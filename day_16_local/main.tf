15944d3850824da8ef1fbb3840cbc7729d7f93d3
hemanthkumarchintada <hemanthkumarchintada>
2024-09-05 23:32:04 +0530

    update

--- day_16_local/main.tf
+++ day_16_local/main.tf
@@ -2,11 +2,14 @@ locals {
   region = "us-east-1"
   environment = "aws"
   instance_type = "t2.micro"
+  key_name ="react"
+  ami="ami-07e35c3920b92d884"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-07e35c3920b92d884" # Amazon Linux 2 AMI
+  ami           = local.ami # Amazon Linux 2 AMI
   instance_type = local.instance_type
+  key_name = local.key_name
 
   tags = {
     Name        = "terraform"
